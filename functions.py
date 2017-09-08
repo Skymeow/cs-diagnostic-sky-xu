277da5d50e14bb3f3fa7e8405af645c3a83f990f
Sky Xu <skyrocks88xmz@gmail.com>
2017-09-07 17:37:20 -0700

    finish function

---
+++ functions.py
@@ -0,0 +1,24 @@
+# Solution to Problem 8
+def fibonacci_iterative(num = 10):
+    for i in (2,num):
+        list.append(i-2, i-1)
+        return i
+
+ fibonacci_iterative(num = 20)
+
+# Solution to Problem 9
+var num = 10
+def fibonacci_iterative(num):
+    list = []
+    for i in (2,num):
+        list.append(i-2, i-1)
+        return list[0:n]
+
+# Solution to Problem 10
+def factorial_recursive(num):
+    if num = 1:
+     return 1
+    else:
+        return factorial_recursive(num)*factorial_recursive(num-1)
+
+
